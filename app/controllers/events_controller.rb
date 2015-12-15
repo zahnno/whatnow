718194997040adfa163862f0c306cb6379121a58
PatrickZanowski <zanowski26@hotmail.com>
2015-12-14 22:50:32 -0500

    Added randomization after scoped results

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ before_filter :load_select
 	def show 
 		@events = Event.where(nil)
 		@events = @events.category(@select.interest)
-		@events = @events.grpsize(@select.group_size)
+		@events = @events.grpsize(@select.group_size).order('random()').limit(1)
 	end
 
 
