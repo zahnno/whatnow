0b418a487868805013dfd32abe3fb2f77e0f94c5
PatrickZanowski <zanowski26@hotmail.com>
2016-01-14 17:36:19 -0500

    Created responsive design for window resize

---
+++ config/initializers/Yelp.client.rb
@@ -0,0 +1,8 @@
+require 'yelp'
+
+Yelp.client.configure do |config|
+  config.consumer_key = "rUmSsjxRl2Fyv4EhLg9Szw"
+  config.consumer_secret = "VRtz2b1u7jtffm5cVVDM1okAtQQ"
+  config.token = "DWoZHgyAA4djbMmPPZ10JG4MuPlYYMW7"
+  config.token_secret = "9hLXstxIzR5stLkkEJT0dXLBMTM"
+end
